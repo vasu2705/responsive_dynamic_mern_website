702fb367605f23350576bc158ce5ccd96f7a41a5
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-08-07 00:44:25 +0530

    fdf

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 //cretating a database
 dotenv.config({ path: "./config.env" });
-// const DB = process.env.DATABASE;
+const DB = process.env.DATABASE;
 mongoose
- 
-.connect("mongodb+srv://responsive_mern:Vasu@1234@cluster0.pwhaa.mongodb.net/responsive_mern?retryWrites=true&w=majority", {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
